28c793dfa1e596ea260581107df697ca0e5a5f32
Omar Montaño Rivas <omarmrivas@Omars-Mac-Pro.local>
2018-06-29 16:20:55 -0500

    First commit

---
+++ GranMorada/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open System.Threading
+open Suave
+
+[<EntryPoint>]
+let main argv = 
+  let cts = new CancellationTokenSource()
+  let conf = { defaultConfig with cancellationToken = cts.Token
+                                  bindings = [ HttpBinding.createSimple HTTP "10.0.1.4" 8082] }
+  let listening, server = startWebServerAsync conf (Successful.OK "Hello World")
+    
+  Async.Start(server, cts.Token)
+  printfn "Make requests now"
+  Console.ReadKey true |> ignore
+    
+  cts.Cancel()
+
+  0    
